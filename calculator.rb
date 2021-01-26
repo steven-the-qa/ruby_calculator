f3b6d00dfd823e5aab533a42513699ec1f2ad89b
Steven Boutcher <boutchersj@gmail.com>
2021-01-26 14:15:11 -0600

    Refactored calculate method to be split into multiple methods, one for each type of operation; Removed the instance variables from the Calculator class

--- calculator.rb
+++ calculator.rb
@@ -2,24 +2,27 @@
 
 # Calculator project to practice Ruby & RSpec
 class Calculator
-  attr_accessor :num1, :operator, :num2
-
-  def initialize
-    @num1 = 0
-    @operator = ''
-    @num2 = 0
+  def add(num1, num2)
+    num1 + num2
   end
 
-  def calculate(first_num, op_symbol, second_num)
-    num1 = first_num
-    operator = op_symbol
-    num2 = second_num
-    case operator
-    when '+' then num1 + num2
-    when '-' then num1 - num2
-    when '*' then num1 * num2
-    when '/' then num1 / num2
-    when '%' then num1 % num2
-    when '^' then num1**num2; end
+  def subtract(num1, num2)
+    num1 - num2
+  end
+
+  def multiply(num1, num2)
+    num1 * num2
+  end
+
+  def divide(num1, num2)
+    num1 / num2
+  end
+
+  def x_to_the_y(num1, num2)
+    num1**num2
+  end
+
+  def modulo(num1, num2)
+    num1 % num2
   end
 end
