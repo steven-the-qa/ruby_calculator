aacbf2d7d65838306a1b774600ed9acaa427f10a
Steven Boutcher <boutchersj@gmail.com>
2021-01-31 16:58:47 -0600

    Added user interaction to the calculator; user can now run the program and give the calculator 2 numbers and an operator

--- calculator.rb
+++ calculator.rb
@@ -26,3 +26,30 @@ class Calculator
     num1 % num2
   end
 end
+
+puts 'Enter the first number'
+first_num = gets.chomp.to_i
+
+puts "From this list, please enter the operation you'd like to perform:"
+puts '+ - / * ^ %'
+operation = gets.chomp
+
+puts 'Enter the second number'
+second_num = gets.chomp.to_i
+
+calculator = Calculator.new
+
+case operation
+when '+'
+  puts "Result is #{calculator.add(first_num, second_num)}"
+when '-'
+  puts "Result is #{calculator.subtract(first_num, second_num)}"
+when '*'
+  puts "Result is #{calculator.multiply(first_num, second_num)}"
+when '/'
+  puts "Result is #{calculator.divide(first_num, second_num)}"
+when '^'
+  puts "Result is #{calculator.x_to_the_y(first_num, second_num)}"
+when '%'
+  puts "Result is #{calculator.modulo(first_num, second_num)}"
+end
